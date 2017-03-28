003c5205f3ec0ad1809e1eef8f16c53c0d040a0b
stannhaeuser <stefantannhauser@Stefans-MacBook-Pro.local>
2017-03-28 01:42:55 -0700

    Added canvas

--- video-editor.js
+++ video-editor.js
@@ -1,11 +1,14 @@
 (function(angular) {
   'use strict';
 function VideoEditorController() {
-
+  var canvas = document.getElementById("video-editor");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0,0,150,75);
 }
 
-angular.module('angular-canvas-video-editor').component('videoEditor', {
-  templateUrl: 'video-editor.html',
+angular.module('angular-canvas-video-editor', []).component('videoEditor', {
+  template: '<canvas id="video-editor" style="border:1px solid #000000;"></canvas>',
   controller: VideoEditorController,
   bindings: {
     hero: '='
